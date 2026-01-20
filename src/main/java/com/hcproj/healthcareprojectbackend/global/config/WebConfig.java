356582158a0c643cbe1f71b319c011ce6a1af5da
Younghoon Choi <ozymandias089@gmail.com>
2026-01-20 12:26:50 +0900

    Remove unused import in WebConfig.java
    Removed unused import for ResourceHandlerRegistry.

--- src/main/java/com/hcproj/healthcareprojectbackend/global/config/WebConfig.java
+++ src/main/java/com/hcproj/healthcareprojectbackend/global/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.hcproj.healthcareprojectbackend.global.security.resolver.CurrentUserA
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
