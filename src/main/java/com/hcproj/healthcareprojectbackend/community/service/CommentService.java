8f568fcfca3ee7d562c92c2d5e6f08dddc746de4
Younghoon Choi <ozymandias089@gmail.com>
2026-01-20 12:24:53 +0900

    Modify updateComment method to include postId
    Updated the updateComment method to include postId as a parameter and added validation for commentId.

--- src/main/java/com/hcproj/healthcareprojectbackend/community/service/CommentService.java
+++ src/main/java/com/hcproj/healthcareprojectbackend/community/service/CommentService.java
@@ -64,9 +64,10 @@ public class CommentService {
     }
 
     @Transactional
-    public CommentUpdateResponseDTO updateComment(Long userId, Long commentId, CommentUpdateRequestDTO request) {
+    public CommentUpdateResponseDTO updateComment(Long userId, Long postId, Long commentId, CommentUpdateRequestDTO request) {
         CommentEntity comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.COMMENT_NOT_FOUND));
+        if (comment.getCommentId() != commentId) = throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE);
 
         if (!comment.getUserId().equals(userId)) throw new BusinessException(ErrorCode.NOT_COMMENT_AUTHOR);
 
