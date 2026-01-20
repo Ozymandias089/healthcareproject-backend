86f8a6664adeab7a70db820c9c6143b9375ec1f3
Younghoon Choi <ozymandias089@gmail.com>
2026-01-20 09:39:32 +0900

    Delete src/main/java/com/hcproj/healthcareprojectbackend/community/dto/request/PostSearchCondition.java

--- src/main/java/com/hcproj/healthcareprojectbackend/community/dto/request/PostSearchCondition.java
+++
@@ -1,9 +0,0 @@
-package com.hcproj.healthcareprojectbackend.community.dto.request;
-
-public record PostSearchCondition(
-        String category,    // ALL, FREE, QUESTION, INFO
-        String searchBy,    // TITLE, CONTENT, TITLE_CONTENT, AUTHOR
-        String q,           // 검색어
-        Long cursorId,      // 마지막으로 본 게시글 ID
-        Integer size        // 가져올 개수 (기본값 처리는 Controller에서)
-) {}
